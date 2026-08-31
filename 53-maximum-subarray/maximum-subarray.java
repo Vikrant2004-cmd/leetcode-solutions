2c50bfbde5b49076310582ff602deb87330a707d
Vikrant2004-cmd <vikrantbhadauria07@gmail.com>
2026-08-31 07:42:53 +0530

    Time: 1 ms (99.94%) | Memory: 77.3 MB (48.97%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentsum = nums[0];
+        int maxsum = nums[0];
+        for(int i = 1;i<nums.length;i++){
+            if(currentsum<0){
+                currentsum=nums[i];
+            }
+            else{
+                currentsum=currentsum+nums[i];
+            }
+            if(currentsum>maxsum){
+                maxsum=currentsum;
+            }
+        }
+
+        return maxsum;
+
+    
+    }
+}

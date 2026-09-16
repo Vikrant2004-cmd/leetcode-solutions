6701afbb7587b16bc20f6a23ecf2bc202b539822
Vikrant2004-cmd <vikrantbhadauria07@gmail.com>
2026-09-16 09:21:14 +0530

    Time: 6 ms (62.18%) | Memory: 42.3 MB (99.34%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = s.length();
+        for(int i = 0; i < s.length();i++){
+            int left = i-1;
+            int right = i+1;
+            while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
+            }
+        }
+        for( int i = 0;i<s.length();i++){
+            int left = i;
+            int right = i+1;
+            while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
+            }
+        }
+        return count;
+
+
+        
+    }
+}
